8df4e6ad97d37b0aa530565c6f412cad03add0f9
unknown <shobhitdimri01>
2022-09-28 07:48:54 +0530

    new code

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -53,97 +53,87 @@ const Resume = (props) => {
 
   //here we have
   const programmingSkillsDetails = [
-    { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "React JS", ratingPercentage: 85 },
-    { skill: "React Native", ratingPercentage: 85 },
-    { skill: "Express JS", ratingPercentage: 89 },
-    { skill: "Node JS", ratingPercentage: 89 },
-    { skill: "Mongo Db", ratingPercentage: 70 },
-    { skill: "Core Java", ratingPercentage: 80 },
-    { skill: "HTML", ratingPercentage: 80 },
-    { skill: "CSS", ratingPercentage: 80 },
+        {skill:"Golang",ratingPercentage:75},
+        {skill:"Core-Java",ratingPercentage:80},
+        {skill:"Python",ratingPercentage:80},
+        {skill:"ReactJS",ratingPercentage:20},
+        {skill:"HTML", ratingPercentage:80},
+        {skill:"Linux", ratingPercentage:80},
+        {skill:"Web-Development",ratingPercentage:75},
+        {skill:"Backend-Development",ratingPercentage:80},
+        {skill:"OOPS",ratingPercentage:80},
+        {skill:"SQL",ratingPercentage:80},
   ];
 
   const projectsDetails = [
     {
       title: "Personal Portfolio Website",
-      duration: { fromDate: "2020", toDate: "2021" },
+      duration : {fromDate:"2021",toDate:"2022"},
       description:
-        "A Personal Portfolio website to showcase all my details and projects at one place.",
-      subHeading: "Technologies Used: React JS, Bootsrap",
-    },
-    {
-      title: "Mobile E-shop ",
-      duration: { fromDate: "2020", toDate: "2021" },
+      "A personal portfolio to display all my professional details & project at one place.",
+      subHeading:"Technologies Used:React JS,Bootstrap"
+  },
+  {
+      title: "Booking Website",
+      duration : {fromDate:"2021",toDate:"2022"},
       description:
-        "An ecommerce application designed to sell products online wth payment system integration",
-      subHeading:
-        "Technologies Used:  React Native, Mongo DB, Express Js, Node Js, Redux.",
-    },
-    {
-      title: "Ecommerce Website ",
-      duration: { fromDate: "2020", toDate: "2021" },
+      "Hotel Website where you can Book-rooms for specific date with admin functionality available.",
+      subHeading:"Fullstack Technology:Golang,PostgreSQL,Bootstrap,chi-router"
+  },
+  {
+      title: "Flask API",
+      duration : {fromDate:"2021",toDate:"2022"},
       description:
-        "Online ecommerce website for showcasing and selling products onlne with payment system integration, both Paypal and Stripe",
-      subHeading:
-        "Technologies Used: Mongo DB, Epress Js, React Js, Node JS, Redux, Bootstrap.",
-    },
+      "REST API created in Flask following using CRUD methodology with authorization and security functionality.",
+      subHeading:"Backend Technology:Flask,JSON,JWT Authentication"
+  },
   ];
 
   const resumeDetails = [
     <div className="resume-screen-container" key="education">
-      <ResumeHeading
-        heading={"University of Legon Accra, Ghana"}
-        subHeading={"BACHELOR OF SCIENCE INFORMATION TECHNOLOGY"}
-        fromDate={"2014"}
-        toDate={"2018"}
-      />
-
-      <ResumeHeading
-        heading={"National Youth Service Corps"}
-        subHeading={"Ministry Of Science And Technogy. Uyo Akwa Ibom State"}
-        fromDate={"2019"}
-        toDate={"2020"}
-      />
-      <ResumeHeading
-        heading={"High School "}
-        subHeading={"Command Secondary School Mbiri"}
-        fromDate={"2007"}
-        toDate={"2012"}
-      />
+     <ResumeHeading
+            heading={"Bipin Tripathi Kumaon Institute of Technology,Uttarakhand"}
+            subHeading={"Bachelors in Technology (B.Tech)"}
+            fromDate={"2017"}
+            toDate={"2021"}
+            />
+            <ResumeHeading
+            heading={"Delhi Public School,Dehradun"}
+            subHeading={"Higher secondary education"}
+            fromDate={"2014"}
+            toDate={"2016"}
+            />
     </div>,
 
     /* WORK EXPERIENCE */
     <div className="resume-screen-container" key="work-experience">
       <div className="experience-container">
-        <ResumeHeading
-          heading={"Ehizeex Technoloy"}
-          subHeading={"FULL STACK DEVELOPER INTERN"}
-          fromDate={"2021"}
-          toDate={"Present"}
-        />
-        <div className="experience-description">
-          <span className="resume-description-text">
-            Currently working as MERN stack web and mobile developer and also an
-            online instructor on udemy.
-          </span>
-        </div>
-        <div className="experience-description">
-          <span className="resume-description-text">
-            - Developed an ecommerce website for client with the dashboard for
-            managing the products, managing reviews, users, payment etc. .
-          </span>
-          <br />
-          <span className="resume-description-text">
-            - Integrated the web app with backend services to create new user
-            onboarding application with dynamic form content.{" "}
-          </span>
-          <br />
-          <span className="resume-description-text">
-            - I stretch my mental capacity to develope UI as per the given
-            designs.
-          </span>
-          <br />
+      <ResumeHeading
+            heading={"Tata Consultancy Service(TCS)"}
+            subHeading={"System Engineer"}
+            fromDate={"2021"}
+            toDate={"present"}
+            />
+            <div className='experience-description'>
+                <span className='resume-description-text'>
+                    Currently working as a Backend Developer in a 5G Research & Development FullStack Project
+                </span>
+            </div>
+
+            <div className='experience-description'>
+                <span className='resume-description-text'>
+               - Working at RAN side developing the feature related to various GNB making API with knoweledge of 3GPP documentation
+                 Implemented class1 procedure messages RAN to AMF with fully dynamic and optimized datastructure
+                </span>
+                <br/>
+                <span className='resume-description-text'>
+                - Developed logging functionality (using gin.context) with better error handling to send API via function from RAN to CORE and providing output response from CORE at Frontend of Tool.{" "}
+                </span>
+                <br/>
+                <span className='resume-description-text'>
+                - Implemented HTTP2 feature at the middleware of the RAN to provide secure transfer of request and also worked on estaballishing SCTP connection b/w different servers
+                 </span>
+                <br/>
         </div>
       </div>
     </div>,
@@ -184,16 +174,16 @@ const Resume = (props) => {
     /* Interests */
     <div className="resume-screen-container" key="interests">
       <ResumeHeading
-        heading="Teaching"
-        description="Apart from being a tech enthusiast and a code writer, i also love to teach people what i know simply because i believe in sharing."
+        heading="Reading Novels"
+        description="Apart from being a tech enthusiast and a code writer, i also love to read novels and self-improvement books."
       />
       <ResumeHeading
-        heading="Music"
-        description="Listening to soothing music is something i can never compromise with, skimming through Spotify's pop songs charts is at times the best stress reliever that i can get my hands on."
+        heading="Watching WebSeries & Anime"
+        description="In my leisure time I love to watch Anime and webseries "
       />
       <ResumeHeading
-        heading="Competitive Gaming"
-        description="I like to challenge my reflexes a lot while competing in football games, pushing the rank and having interactive gaming sessions excites me the most."
+        heading="Cooking"
+        description="I like to cook and experiment with different variety of foods that soothe my taste and I find it a relaxing and fun-loving activity "
       />
     </div>,
   ];
